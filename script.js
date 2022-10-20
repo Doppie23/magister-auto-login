dbb35513a1a2b86f2e2b4aa873ad4e3760e08189
Doppie22 <dvopstal05@gmail.com>
2022-10-20 21:05:37 +0200

    werkt

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 waitForElm('#username').then(() => {
-  console.log('Element is ready');
-  document.getElementById('username').value = "7661";
-  document.getElementById('username').dispatchEvent(new Event('input'));
+  chrome.storage.sync.get(['leerlingnummer'], function(leerlingnummer){
+    document.getElementById('username').value = leerlingnummer.leerlingnummer;
+    document.getElementById('username').dispatchEvent(new Event('input'));
 
-  waitForElm('#username_submit').then((elm) => {
-    console.log(elm)
-    elm.click();
-  });
+    waitForElm('#username_submit').then((elm) => {
+      elm.click();
+    });
+  });  
 });
 
 waitForElm('#password').then(() => {
-  console.log('Element is ready');
-  document.getElementById('password').value = "Dropshot#1";
-  document.getElementById('password').dispatchEvent(new Event('input'));
-
-  waitForElm('#password_submit').then((elm) => {
-    elm.click();
+  chrome.storage.sync.get(['wachtwoord'], function(wachtwoord){
+    document.getElementById('password').value = wachtwoord.wachtwoord;
+    document.getElementById('password').dispatchEvent(new Event('input'));
+  
+    waitForElm('#password_submit').then((elm) => {
+      elm.click();
+    });
   });
 });
 
