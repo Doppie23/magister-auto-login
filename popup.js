5a70bccabc2e0af1622be2733f57bb5e1288a93e
Doppie22 <dvopstal05@gmail.com>
2022-10-20 20:44:24 +0200

    gui

---
+++ popup.js
@@ -0,0 +1,61 @@
+document.getElementById('save').addEventListener('click', save_options);
+document.addEventListener('DOMContentLoaded', restore_options);
+
+function save_options() {
+    const leerlingnummer = document.querySelector('.leerlingnummer').value;
+    if (leerlingnummer === ''){
+        console.log('empty');
+        document.querySelector('.leerlingnummer').classList.add('empty')
+
+        var status = document.getElementById('status');
+        status.textContent = 'Type leerlingnummer in.';
+        setTimeout(function() {
+        status.textContent = '';
+        }, 2000);
+        var leercheck = false
+    }
+    else {     
+        console.log(leerlingnummer)   
+        chrome.storage.sync.set({leerlingnummer: leerlingnummer});
+        var leercheck = true        
+    }
+
+    const wachtwoord = document.querySelector('.wachtwoord').value;
+    if (wachtwoord === ''){
+        console.log('empty');
+        document.querySelector('.wachtwoord').classList.add('empty')
+
+        var status = document.getElementById('status');
+        status.textContent = 'Type wachtwoord in.';
+        setTimeout(function() {
+        status.textContent = '';
+        }, 2000);
+        var wwcheck = false
+    }
+    else {
+        chrome.storage.sync.set({wachtwoord: wachtwoord});
+        var wwcheck = true
+    }
+
+    if (leercheck == true && wwcheck == true){
+        var status = document.getElementById('savebericht');
+        status.textContent = 'Opgeslagen.';
+
+        document.querySelector('.wachtwoord').classList.remove('empty')
+        document.querySelector('.leerlingnummer').classList.remove('empty')
+
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    }
+}
+
+function restore_options() {
+    chrome.storage.sync.get(['leerlingnummer'], function(leerlingnummer){
+        console.log(leerlingnummer)
+        document.querySelector('.leerlingnummer').value = leerlingnummer.leerlingnummer;
+    })
+    chrome.storage.sync.get(['wachtwoord'], function(wachtwoord){
+        document.querySelector('.wachtwoord').value = wachtwoord.wachtwoord;
+    })
+}
